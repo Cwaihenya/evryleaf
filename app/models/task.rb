4aba40587fa66a8995e3636eb4754fef1f07ae13
Cwaihenya <cwaihenya.gmail.com>
2021-07-05 17:32:03 +0900

    database validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,8 @@
 class Task < ApplicationRecord
+validates_presence_of :title,
+        message: "Title should be included"
+validates_presence_of :content,
+        message: "Content should be provided"
+validates_presence_of :deadline,
+                  message: "Task_deadline should be listed"
 end
